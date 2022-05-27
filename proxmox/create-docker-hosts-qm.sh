8e8d1ca85b21adc11b863043503a9329b80a7047
Rusty Tyner <rtyner@users.noreply.github.com>
2022-05-27 13:59:57 -0400

    removed storage flag

--- proxmox/create-docker-hosts-qm.sh
+++ proxmox/create-docker-hosts-qm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-qm clone 1000 200 --name rt-prod-docker-1 --pool ssd-pool
+qm clone 1000 200 --name rt-prod-docker-1
 qm set 200 --sshkeys /root/.ssh/id_ed25519.pub
 qm set 200 --ipconfig0 ip=10.1.1.200/24,gw=10.1.1.1
 qm resize 200 scsi0 +20G
