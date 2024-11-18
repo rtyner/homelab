e7b700e03ad7fb7c65560eb73657580724b65a29
rt <rusty@rtyner.com>
2024-11-17 20:36:03 -0500

    updated dir structure

--- Infrastructure/prod-truenas-01.local.rtyner.com/qm-scripts/ubuntu/create-debfile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-qm clone 1001 105 --name nfs-file-1
-qm set 105 --sockets 2 --cores 4 --memory 8096 --sshkeys /root/.ssh/id_ed25519.pub --ciuser rt --ipconfig0 ip=10.1.1.22/24,gw=10.1.1.1 --autostart 1
-qm resize 105 scsi0 +60G
-qm start 105
--- Infrastructure/prod-truenas-01.local.rtyner.com/qm-scripts/ubuntu/create-docker.sh
+++
@@ -1,4 +0,0 @@
-qm clone 1001 214 --name prod-docker-3
-qm set 214 --sockets 2 --cores 4 --memory 8096 --sshkeys /root/.ssh/id_ed25519.pub --ciuser rt --ipconfig0 ip=10.1.1.12/24,gw=10.1.1.1 --autostart 1
-qm resize 214 scsi0 +22G
-qm start 216
