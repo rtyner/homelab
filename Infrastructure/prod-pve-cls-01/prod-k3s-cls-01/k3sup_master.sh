0b04564c225c0c00a48196597eab18ac877f2bbd
rt <rusty@rtyner.com>
2024-12-01 15:07:42 -0500

    updated script to perform all k3sup tasks

--- Infrastructure/prod-pve-cls-01/prod-k3s-cls-01/k3sup_master.sh
+++ Infrastructure/prod-pve-cls-01/prod-k3s-cls-01/k3sup_master.sh
@@ -1,10 +1,24 @@
 k3sup install \
 --ip 10.1.1.50 \
---tls-san 10.1.1.75 \
---tls-san prod-k3s-cls01-vip-01.local.rtyner.com \
---cluster \
---k3s-channel latest \
---k3s-extra-args "--disable servicelb --disable traefik" \
---local-path $HOME/.kube/config \
 --user rt \
---merge
+--ssh-key ~/.ssh/id_ed25519 \
+--sudo \
+--k3s-channel latest \
+--tls-san 10.1.1.10 \
+--tls-san prod-k3s-cls01-vip-01.local.rtyner.com \
+ --cluster --local-path ~/.kube/k8s-cluster.dev.dman.cloud.yaml \
+ --context k8s-cluster-ha \
+ --k3s-extra-args "--disable traefik --disable servicelb --node-ip=10.1.1.50"
+
+kubectl apply -f https://kube-vip.io/manifests/rbac.yaml 
+
+alias kube-vip="ctr run --rm --net-host docker.io/plndr/kube-vip:latest vip /kube-vip"
+
+kube-vip manifest daemonset \
+--arp \
+--interface eth0 \
+--address 10.1.1.10 \
+--controlplane \
+--leaderElection \
+--taint \
+--inCluster | tee /var/lib/rancher/k3s/server/manifests/kube-vip.yaml
