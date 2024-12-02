3744016183586c7e2667099abcc86f1bf4c46947
rt <rusty@rtyner.com>
2024-12-01 19:39:34 -0500

    changed from latest to stable branch

--- Infrastructure/prod-pve-cls-01/prod-k3s-cls-01/k3sup_master.sh
+++ Infrastructure/prod-pve-cls-01/prod-k3s-cls-01/k3sup_master.sh
@@ -3,16 +3,17 @@ k3sup install \
 --user rt \
 --ssh-key ~/.ssh/id_ed25519 \
 --sudo \
---k3s-channel latest \
+--k3s-channel stable \
 --tls-san 10.1.1.10 \
---tls-san prod-k3s-cls01-vip-01.local.rtyner.com \
- --cluster --local-path ~/.kube/k8s-cluster.dev.dman.cloud.yaml \
- --context k8s-cluster-ha \
- --k3s-extra-args "--disable traefik --disable servicelb --node-ip=10.1.1.50"
+--cluster --local-path ~/.kube/k8s-cluster.dev.dman.cloud.yaml \
+--context k8s-cluster-ha \
+--k3s-extra-args "--disable traefik --disable servicelb --node-ip=10.1.1.50"
 
 kubectl apply -f https://kube-vip.io/manifests/rbac.yaml 
 
 alias kube-vip="ctr run --rm --net-host docker.io/plndr/kube-vip:latest vip /kube-vip"
+
+ctr image pull docker.io/plndr/kube-vip:latest
 
 kube-vip manifest daemonset \
 --arp \
@@ -22,3 +23,12 @@ kube-vip manifest daemonset \
 --leaderElection \
 --taint \
 --inCluster | tee /var/lib/rancher/k3s/server/manifests/kube-vip.yaml
+
+k3sup join --ip 10.1.1.51 --user rt --ssh-key ~/.ssh/id_ed25519 \ --sudo --k3s-channel stable --server --server-ip 10.1.1.10 --server-user rt --sudo --k3s-extra-args "--disable traefik  --disable servicelb --node-ip=10.1.1.51 --token={$K3S_TOKEN}"
+k3sup join --ip 10.1.1.52 --user rt --ssh-key ~/.ssh/id_ed25519 \ --sudo --k3s-channel stable --server --server-ip 10.1.1.10 --server-user rt --sudo --k3s-extra-args "--disable traefik  --disable servicelb --node-ip=10.1.1.52 --token={$K3S_TOKEN}"
+
+k3sup join --user rt --ssh-key ~/.ssh/id_ed25519 --sudo --server-ip 10.1.1.10 --ip 10.1.1.53 --k3s-channel stable -- --k3s-extra-args "--disable traefik --disable servicelb" --print-command
+k3sup join --user rt --ssh-key ~/.ssh/id_ed25519 --sudo --server-ip 10.1.1.10 --ip 10.1.1.54 --k3s-channel stable -- --k3s-extra-args "--disable traefik --disable servicelb" --print-command
+k3sup join --user rt --ssh-key ~/.ssh/id_ed25519 --sudo --server-ip 10.1.1.10 --ip 10.1.1.55 --k3s-channel stable -- --k3s-extra-args "--disable traefik --disable servicelb" --print-command
+k3sup join --user rt --ssh-key ~/.ssh/id_ed25519 --sudo --server-ip 10.1.1.10 --ip 10.1.1.56 --k3s-channel stable -- --k3s-extra-args "--disable traefik --disable servicelb" --print-command
+k3sup join --user rt --ssh-key ~/.ssh/id_ed25519 --sudo --server-ip 10.1.1.10 --ip 10.1.1.57 --k3s-channel stable -- --k3s-extra-args "--disable traefik --disable servicelb" --print-command
