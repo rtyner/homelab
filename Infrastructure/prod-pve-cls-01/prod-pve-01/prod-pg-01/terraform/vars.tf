2c516f98488a8cc109d40543e6c7cee6a9482fd4
rt <rusty@rtyner.com>
2024-12-15 13:08:14 -0500

    updated vmid

--- Infrastructure/prod-pve-cls-01/prod-pve-01/prod-pg-01/terraform/vars.tf
+++ Infrastructure/prod-pve-cls-01/prod-pve-01/prod-pg-01/terraform/vars.tf
@@ -11,39 +11,40 @@ variable "cipassword" {
 }
 
 variable "pm_api_token_id" {
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
   description = "Proxmox API token ID"
 }
 
 variable "pm_api_token_secret" {
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
   description = "Proxmox API token secret"
 }
 
 variable "vms" {
-    type = map(object({
-      name          = string
-      ip_address    = string
-      cores         = number 
-      sockets       = number
-      vmid          = number
-      memory        = number #mb
-      disk_space    = number #gb 
-      start_at_boot = bool
-    }))
-  
+  type = map(object({
+    name          = string
+    ip_address    = string
+    cores         = number
+    sockets       = number
+    vmid          = number
+    memory        = number #mb
+    disk_space    = number #gb 
+    start_at_boot = bool
+  }))
+
   default = {
     prod-pg-01 = {
-        name          = "prod-pg-01"
-        ip_address    = "10.1.1.15"
-        cores         = 8
-        sockets       = 1
-        memory        = 16384
-        disk_space    = 128
-        vmid          = 109
-        start_at_boot = true
-    }  
+      name          = "prod-pg-01"
+      ip_address    = "10.1.1.15"
+      cores         = 8
+      sockets       = 1
+      memory        = 16384
+      disk_space    = 128
+      vmid          = 104
+      start_at_boot = true
+    }
   }
 }
+
